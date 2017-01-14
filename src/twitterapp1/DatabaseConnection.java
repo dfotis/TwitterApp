c438f09cd194c65c1191a53527f482f1be076c7b
ManosMagnus <manolis_kirt@yahoo.gr>
2017-01-14 21:42:51 +0200

    Completed part 3

--- src/twitterapp1/DatabaseConnection.java
+++ src/twitterapp1/DatabaseConnection.java
@@ -1,21 +1,68 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * Created by manos on 9/1/2017.
  */
 public class DatabaseConnection {
+    String url = "jdbc:mysql://localhost:3306/javabase";
+    String username = "java";
+    String password = "password";
 
     DatabaseConnection(){
-        String url = "jdbc:mysql://localhost:3306/javabase";
-        String username = "java";
-        String password = "password";
+
+    }
+
+    void insertRecord(DatabaseRecord record)  {
+
 
         System.out.println("Connecting database...");
 
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+        try ( Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("Database connected!");
+
+            record.insertUser("users",connection);
+            record.insertHashtags("hashtags",connection);
+            record.insertMentions("mentions",connection);
+            record.insertRetweet("retweet",connection);
+            record.insertTweet("tweet",connection);
+            record.insertUrls("urls",connection);
+
+//
+//           // String query_user = record.insertUser("users");
+//            //System.out.println("Users query: " + query_user);
+//           // Statement stmt = connection.createStatement();
+//            //stmt.executeUpdate(query_user);
+//
+//            String query_tweet = record.insertTweet("tweet");
+//            System.out.println("Tweet query: " + query_tweet);
+//            stmt.executeUpdate(query_tweet);
+//
+//            String query_retweet = record.insertRetweet("retweet");
+//            System.out.println("ReTweet query: " + query_retweet);
+//            if(query_retweet != ""){
+//                stmt.executeUpdate(query_retweet);
+//            }
+//
+//
+//            ArrayList<String> query_hashtags = record.insertHashtags("hashtags");
+//            for(String hashtag : query_hashtags){
+//                System.out.println("Hashtag query: " + hashtag);
+//                stmt.executeUpdate(hashtag);
+//            }
+//
+//            ArrayList<String> query_mentions = record.insertMentions("mentions");
+//            for (String mention : query_mentions){
+//                System.out.println("Mention query: " + mention);
+//                stmt.executeUpdate(mention);
+//            }
+//
+//            ArrayList<String> query_urls = record.insertUrls("urls");
+//            for (String url : query_urls){
+//                System.out.println("URLS query: " + url);
+//                stmt.executeUpdate(url);
+//            }
+
         } catch (SQLException e) {
             throw new IllegalStateException("Cannot connect the database!", e);
         }
--- src/twitterapp1/DatabaseRecord.java
+++ src/twitterapp1/DatabaseRecord.java
@@ -1,19 +1,27 @@
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 import java.net.URL;
+import java.sql.*;
+import java.util.Date;
 import java.util.ArrayList;
 import java.util.StringJoiner;
+import java.util.TimeZone;
 
 /**
  * Created by manos on 9/1/2017.
  */
 public class DatabaseRecord {
-    int user_id;
+    String user_id;
     String user_name;
     long timestamp;
     boolean retwitte;
     ArrayList<String> url_list;
     ArrayList<String> expanded_url_list;
     ArrayList<String> mention_name_list;
-    ArrayList<Long> mention_id_list;
+    ArrayList<String> mention_id_list;
     ArrayList<String> hashtags_list;
     String text;
 
@@ -27,7 +35,7 @@ public class DatabaseRecord {
 
     }
 
-    void setUser(int user_id, String user_name){
+    void setUser(String user_id, String user_name){
         this.user_id = user_id;
         this.user_name = user_name;
     }
@@ -41,7 +49,7 @@ public class DatabaseRecord {
         expanded_url_list.add(expanded_url);
     }
 
-    void addMentions(String name, long id ){
+    void addMentions(String name, String id ){
         mention_id_list.add(id);
         mention_name_list.add(name);
     }
@@ -56,5 +64,76 @@ public class DatabaseRecord {
 
     void addHashtag(String hashtag){
         hashtags_list.add(hashtag);
+    }
+
+    void insertUser(String table, Connection con) throws SQLException {
+        String query = "INSERT IGNORE INTO " + table + " SET user_id = \"" + user_id + "\", name = \"" + user_name + "\"";
+        Statement stmt = con.createStatement();
+        stmt.executeUpdate(query);
+    }
+
+    void insertTweet(String table, Connection con) throws SQLException {
+        String query = "INSERT INTO " + table + "( user_id, timestamp ,text) VALUES(?,?,?)";
+        PreparedStatement preparedStat = con.prepareStatement(query);
+        preparedStat.setString(1,user_id);
+        preparedStat.setTimestamp(2, Timestamp.valueOf(timestampConverter()));
+        preparedStat.setString(3,text);
+        preparedStat.execute();
+    }
+
+    void insertHashtags(String table, Connection con) throws SQLException {
+        String query = "INSERT INTO " + table + "(user_id, timestamp, hashtag) VALUES(?,?,?)";
+        for (String hashtag : hashtags_list){
+            PreparedStatement preparedStat = con.prepareStatement(query);
+            preparedStat.setString(1,user_id);
+            preparedStat.setTimestamp(2, Timestamp.valueOf(timestampConverter()));
+            preparedStat.setString(3,hashtag);
+            preparedStat.execute();
+        }
+    }
+
+    void  insertMentions(String table, Connection con) throws SQLException {
+        String query = "INSERT INTO " + table + "(user_id,timestamp,mentioned_user) VALUES(?,?,?)";
+        for (String mention : mention_id_list){
+            PreparedStatement preparedStat = con.prepareStatement(query);
+            preparedStat.setString(1,user_id);
+            preparedStat.setTimestamp(2, Timestamp.valueOf(timestampConverter()));
+            preparedStat.setString(3,mention);
+            preparedStat.execute();
+        }
+    }
+
+    void insertUrls(String table, Connection con) throws SQLException {
+        String query = "INSERT INTO " + table + "(user_id, timestamp, url, extended_url) VALUES(?,?,?,?)";
+        for (int i = 0; i < url_list.size(); i++){
+            PreparedStatement preparedStat = con.prepareStatement(query);
+            preparedStat.setString(1,user_id);
+            preparedStat.setTimestamp(2, Timestamp.valueOf(timestampConverter()));
+            preparedStat.setString(3,url_list.get(i));
+            preparedStat.setString(4,expanded_url_list.get(i));
+            preparedStat.execute();
+
+        }
+    }
+
+    void insertRetweet(String table, Connection con) throws SQLException {
+        String query = "INSERT INTO " + table + "(user_id, timestamp ,tweet_user ) VALUES(?,?,?)";
+        if(retwitte){
+            PreparedStatement preparedStat = con.prepareStatement(query);
+            preparedStat.setString(1,user_id);
+            preparedStat.setTimestamp(2, Timestamp.valueOf(timestampConverter()));
+            preparedStat.setString(3,mention_id_list.get(0));
+            preparedStat.execute();
+
+        }
+    }
+
+
+
+    String timestampConverter(){
+        DateTime current_time = new DateTime(timestamp, DateTimeZone.forTimeZone(java.util.TimeZone.getTimeZone("GR/Central")));
+        DateTimeFormatter parser1 = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+        return  parser1.print(current_time);
+
     }
 }
